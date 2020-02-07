7abe529dfd6566b8ec41d4a7d4a437da687129f2
Atsu-Imo <runo1go@gmail.com>
2020-02-08 03:36:26 +0900

    動くようになった

--- src/components/Channels.tsx
+++ src/components/Channels.tsx
@@ -1,20 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import IChannels from "../type/IChannels"
+
+type RequestInfo  = {
+  url :string;
+};
+
+async function http<T>(request: RequestInfo): Promise<T> {
+  const response = await fetch(request.url);
+  const body = await response.json();
+  return body;
+}
 
 const Channels = () => {
-  const [channels, setChannels] = useState<any>([]);
-  const f = async() => {
-    const response = await fetch("http://localhost:1323/channels", {
-      mode: "cors"
+  const [channels, setChannels] = useState<IChannels[]>([]);
+  let channelsList: IChannels[];
+  useEffect(() => {
+    const requestInfo: RequestInfo =  {
+      url: "http://localhost:1323/channels"
+    };
+    const data = http<IChannels[]>(requestInfo);
+    data.then((res) => {
+      channelsList =  res.filter(ch => ch.Title != null);
+      setChannels(channelsList);
     });
-    const json = await response.json;
-    return json;
-  }
-  let chan;
-  f().then(data => {
-    chan = JSON.stringify(data);
-  });
-  setChannels(chan);
-
-  return <div>{channels}</div>;
+  }, []);
+  return <ul>{channels.map(ch => <li>{ch.Title}</li>)}</ul>;
 };
 export default Channels;
---
+++ src/type/IChannels.ts
@@ -0,0 +1,15 @@
+interface IChannel {
+  ID: number,
+  CreatedAt: string,
+  UpdatedAt: string,
+  DeletedAt: string | null,
+  ChannelID: string,
+  Name: string | null,
+  Title: string | null,
+  URL: string,
+  Thumbnail: string,
+  Category: number,
+  Rotation: number
+}
+
+export default IChannel;
