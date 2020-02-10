850bfe1e1dee95469953d0e8b89a8608a12f7286
Atsu-Imo <runo1go@gmail.com>
2020-02-11 01:08:14 +0900

    ページング機能未実装

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -8,12 +8,17 @@ type videoProp = {
 const Videos = (prop: videoProp) => {
   return (
     <div className="videos">
-      {prop.videos.map((video) => {
+      {prop.videos.map(video => {
         return (
           <div key={video.VideoID} className="column">
-            {video.Title}
+            <iframe
+              width="560"
+              height="315"
+              src={"https://www.youtube.com/embed/" + video.VideoID}
+              allow="autoplay; encrypted-media"
+            ></iframe>
           </div>
-        )
+        );
       })}
     </div>
   );
